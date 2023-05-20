c250c3cc122df6a0fc15a06ed71e62ed74498219
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-05-20 12:39:57 -0400

    add constructor

--- js/app.js
+++ js/app.js
@@ -10,67 +10,83 @@
 // good with other dogs
 // breed
 
-let dog1 = {
-  name: "Oso",
-  breed: "Rottweiler",
-  isGoodWithKids: false,
-  isGoodWithOtherDogs: true,
-  isGoodWithCats: false,
-  isAggressive: true,
-  tricks: ["spin", "roll over", "fetch"],
-  age: 0,
-  imgSrc: "images/oso.jpg"
+// Prototype of Dog
+// Constructor (like a factory)
+function Dog(name, breed, age, tricks) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.tricks = tricks;
 }
+
+let dog1 = new Dog("Oso", "Rottweiler", 2, ["spin", "roll over", "fetch"]);
+let dog2 = new Dog("Rover", "Lab", 5, []);
+let dog3 = new Dog("Coffee", "Beagle", 7, ["shake"]);
+console.log(dog1, dog2, dog3);
+
+
+// Object literal
+// let dog1 = {
+//   name: "Oso",
+//   breed: "Rottweiler",
+//   isGoodWithKids: false,
+//   isGoodWithOtherDogs: true,
+//   isGoodWithCats: false,
+//   isAggressive: true,
+//   tricks: ["spin", "roll over", "fetch"],
+//   age: 0,
+//   imgSrc: "images/oso.jpg"
+// }
 
 // TODO: dynamically generate dog objects using form data
 
-'use strict';
+// 'use strict';
 
-let parent = document.getElementById("dogProfiles");
+// let parent = document.getElementById("dogProfiles");
 
-let dog = dog1;
-let dogProfile = document.createElement("article");
-let dogH2 = document.createElement("h2");
-dogH2.textContent = dog.name;
-dogProfile.appendChild(dogH2);
+// let dog = dog1;
+// let dogProfile = document.createElement("article");
+// let dogH2 = document.createElement("h2");
+// dogH2.textContent = `${dog.name} -- ${dog.breed}`;
+// dogProfile.appendChild(dogH2);
 
-if(dog.isGoodWithOtherDogs) {
-  let dogP = document.createElement("p");
-  dogP.textContent = `${dog.name} is good with other dogs.`;
-  dogProfile.appendChild(dogP);
-}
+// if(dog.isGoodWithOtherDogs) {
+//   let dogP = document.createElement("p");
+//   dogP.textContent = `${dog.name} is good with other dogs.`;
+//   dogProfile.appendChild(dogP);
+// }
 
-if(dog.isGoodWithKids) {
-  let dogP = document.createElement("p");
-  dogP.textContent = `${dog.name} is good with kids.`;
-  dogProfile.appendChild(dogP);
-}
+// if(dog.isGoodWithKids) {
+//   let dogP = document.createElement("p");
+//   dogP.textContent = `${dog.name} is good with kids.`;
+//   dogProfile.appendChild(dogP);
+// }
 
-if(dog.isGoodWithCats) {
-  let dogP = document.createElement("p");
-  dogP.textContent = `${dog.name} is good with cats.`;
-  dogProfile.appendChild(dogP);
-}
-if(dog.isAggressive) {
-  let dogP = document.createElement("p");
-  dogP.textContent = `CAUTION: ${dog.name} is aggresive with strangers.`;
-  dogProfile.appendChild(dogP);
-}
+// if(dog.isGoodWithCats) {
+//   let dogP = document.createElement("p");
+//   dogP.textContent = `${dog.name} is good with cats.`;
+//   dogProfile.appendChild(dogP);
+// }
+// if(dog.isAggressive) {
+//   let dogP = document.createElement("p");
+//   dogP.textContent = `CAUTION: ${dog.name} is aggresive with strangers.`;
+//   dogProfile.appendChild(dogP);
+// }
 
-let dogImg = document.createElement("img");
-dogImg.src = dog.imgSrc;
-dogImg.style.height = "150px";
-dogProfile.appendChild(dogImg);
+// let dogImg = document.createElement("img");
+// dogImg.src = dog.imgSrc;
+// dogImg.style.height = "150px";
+// dogProfile.appendChild(dogImg);
 
-let dogUL = document.createElement("ul");
-for(let i=0; i<dog.tricks.length; i++) {
-  let dogLI = document.createElement("li");
-  dogLI.textContent = dog.tricks[i];
-  dogUL.appendChild(dogLI);
-}
-dogProfile.appendChild(dogUL);
+// let dogUL = document.createElement("ul");
+// for(let i=0; i<dog.tricks.length; i++) {
+//   let dogLI = document.createElement("li");
+//   dogLI.textContent = dog.tricks[i];
+//   dogUL.appendChild(dogLI);
+// }
+// dogProfile.appendChild(dogUL);
 
-parent.appendChild(dogProfile);
+// parent.appendChild(dogProfile);
 
 // attach those elements to the parent in the DOM.
 
